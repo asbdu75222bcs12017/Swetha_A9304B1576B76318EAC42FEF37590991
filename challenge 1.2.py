3fad59a94f2c8ef5b95216b1442d08c0bf80f09a
asbdu75222bcs12017 <144210217+asbdu75222bcs12017@users.noreply.github.com>
2023-09-06 12:44:06 +0530

    Add files via upload

---
+++ challenge 1.2.py
@@ -0,0 +1,8 @@
+def isleapyear(year):
+  if(year%4==0):
+    print('{} is a leap year'.format(year))
+  else:
+    print('{} is not a leap year'.format(year))
+year=int(input("Enter a year value"))
+isleapyear(year)
+
